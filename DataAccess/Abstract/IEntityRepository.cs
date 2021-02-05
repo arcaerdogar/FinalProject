58082dc1e9b32d66b4d775d5532c57b7f920f01c
İsmet Arca Erdoğar <arcaerdogar@gmail.com>
2021-02-05 21:46:54 +0300

    05.02.2021 EntityFramework added

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint
+    //class : referance
+    //IEntity veya onu implemente eden bir nesne
+    //new() : newlenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,80 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Oracla,Sql Server,Postgres,MongoDB
+            _products = new List<Product> { 
+                new Product{ProductID=1,CategoryID=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductID=2,CategoryID=1,ProductName="Kamera",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductID=3,CategoryID=2,ProductName="Telefon",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductID=4,CategoryID=2,ProductName="Klavye",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductID=5,CategoryID=2,ProductName="Fare",UnitPrice=15,UnitsInStock=15}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = null;
+            //foreach (var p in _products)//Bu satır yerine linq kullandık
+            //{
+            //    if (product.ProductID==p.ProductID)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+
+            productToDelete = _products.SingleOrDefault(p=>p.ProductID==product.ProductID);
+
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryID)
+        {
+            return _products.Where(p => p.CategoryID == categoryID).ToList();
+
+
+        }
+
+        public void Update(Product product)
+        {
+            //gönderdiğim ürün ıdsine sahip listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryID = product.CategoryID;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
